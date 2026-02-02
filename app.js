ac53366261615e673cfbefcd8e5f35feda68bf44
Sanket01-debug <sanketkansal2001@gmail.com>
2026-02-03 00:30:28 +0530

    feat:Implement real-time location sharing and user disconnection handling

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const path = require("path"); 
+const path = require("path");
 
 const http = require("http");
 
@@ -11,8 +11,14 @@ const io = socketio(server);
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-io.on("connection", function(socket){
-    console.log("connected");
+io.on("connection", function (socket) {
+    socket.on("send-location", function (data) {
+        io.emit("receive-location", { id: socket.id, ...data });
+    });
+
+    socket.on("disconnect", function () {
+        io.emit("user-disconnected", socket.id);
+    })
 });
 
 app.get("/", function (req, res) {
